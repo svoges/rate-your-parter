536377eefb930ae6adee1e283dd460d518c63527
Steffan Voges <steffan@berkeley.edu>
2014-11-20 09:49:50 -0800

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+
+# Make Reviews
+%w(Apple Banana Carrot Date).each do |reviewer|
+  Review.create reviewer: reviewer, rating: 5, description: "Great partner!",
+  person_being_reviewed: "Ryan@berkeley.edu", lecture_id: 2
+end
+
+# Make Users
+%w(Ryan Jenna Daniel Steffan).each do |name|
+  User.create name: name, email: name+"@berkeley.edu"
+end
+
+# Make Lectures
+%w(Algorithms Rails Music Operating\ Systems).each do |name|
+  Lecture.create name: name, semester: "Fall", year: 2014, display_name: name + " Fall 2014"
+end
