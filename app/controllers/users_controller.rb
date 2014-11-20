045210ec163ef8e7be8a7af4683cd5736cd346c7
Steffan Voges <steffan@berkeley.edu>
2014-11-20 11:56:46 -0800

    ability to view any user. will come in handy when we want to see all reviews for a certain user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
-    end
   end
 
 end
