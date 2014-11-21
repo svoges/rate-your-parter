c449628a85b5f79e91b91df96fa3052f624d93cb
Steffan Voges <steffan@berkeley.edu>
2014-11-21 09:13:56 -0800

    ability to edit reviews you created implemented

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,34 @@ class ReviewsController < ApplicationController
     @person_being_reviewed = User.find(@review.person_reviewed_id)
   end
 
+  def edit
+    @users = User.all
+    @lectures = Lecture.all
+    @review = Review.find(params[:id])
+    puts "====="
+    puts @review.lecture_name
+  end
+
   def new
     @users = User.all
     @lectures = Lecture.all
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.rating = update_review_rating
+    @review.description = update_review_description
+    if not @review.errors.full_messages.empty?
+      flash[:error] = @review.errors.full_messages.to_sentence
+      redirect_to edit_review_path
+    elsif @review.rating < 0 or @review.rating > 5
+      flash[:error] = "Rating must be in the range 0-5"
+      redirect_to edit_review_path
+    else
+      @review.save
+      redirect_to lectures_path
+    end
+
   end
 
   def create
@@ -39,6 +64,16 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:description, :rating, :person_being_reviewed, :lecture_name)
+  end
+
+  def update_review_description
+    params.require(:review).permit(:description)[:description]
+  end
+
+  def update_review_rating
+    puts "========update======"
+    puts params[:review][:rating]
+    params.require(:review).permit(:rating)[:rating]
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  post '/reviews/:id', to:'reviews#update'
   devise_for :users
   resources :users
   resources :lectures
