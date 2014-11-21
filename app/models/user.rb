9093c0c28c21a405007370afdd86b163b531187c
Steffan Voges <steffan@berkeley.edu>
2014-11-21 07:55:56 -0800

    fucked that one up, woops

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :given_reviews, class_name: "Review", foreign_key: "reviewer_id"
   has_many :received_reviews, class_name: "Review", foreign_key: "person_reviewed_id"
-  validates :email, format: { with: /\A[a-zA-Z]+"@berkeley.edu"\z/, message: ": only @berkeley.edu emails allowed at this time"}
+  validates :email, format: { with: /\A\w+@berkeley.edu\z/, message: ": only @berkeley.edu emails allowed at this time"}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
